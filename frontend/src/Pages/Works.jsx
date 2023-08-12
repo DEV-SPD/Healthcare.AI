6213996b2ed77a527991ab6c157f3b3a1c044109
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-12 18:33:59 +0530

    How it works page done

---
+++ frontend/src/Pages/Works.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar'
+import { Steps } from '../Components/Steps'
+import { Footer } from '../Components/Footer'
+
+export const Works = () => {
+    return (
+        <>
+            <Navbar />
+            <Steps />
+            <Footer />
+        </>
+    )
+}
