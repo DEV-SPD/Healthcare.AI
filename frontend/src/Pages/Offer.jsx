6df0a8225e3a278f9420198194584236da8634eb
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-12 18:34:19 +0530

    Features Page done

---
+++ frontend/src/Pages/Offer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar'
+import { Footer } from '../Components/Footer'
+import { Features } from '../Components/Features'
+
+export const Offer = () => {
+    return (
+        <>
+            <Navbar />
+            <Features />
+            <Footer />
+        </>
+    )
+}
