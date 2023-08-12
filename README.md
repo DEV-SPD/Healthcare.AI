1346abe020e6935290d0e17ccc433b2b0fb44f99
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-12 17:55:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# code-catalyst 
+Our Track : HEALTHCARE
+
+## HEALTHCARE.AI
+It is a AI-powered diagnostic system that utilizes artificial intelligence (AI) and machine learning algorithms to assist in diagnosing medical conditions and diseases. The goal of AI-powered diagnostics is to enhance the accuracy, speed, and efficiency of the diagnostic process, ultimately leading to improved patient outcomes and more effective healthcare delivery.
+
+## HOW TO START APP:
+1. pip install virtualenv
+2. virtualenv env
+3. .\env\Scripts\activate.ps1
+4. pip install flask
+5. pip install flask_cors
+6. python app.py
---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './Pages/Home';
+import { Works } from './Pages/Works';
+import { Input } from './Pages/Input';
+import { About } from './Pages/About';
+import { Offer } from './Pages/Offer';
+import { Technology } from './Pages/Technology';
+
+import { useState } from 'react';
+import { Result } from './Pages/Result';
+
+function App() {
+  const [result, setResult] = useState({});
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/works' element={<Works />} />
+        <Route path='/liver' element={<Input path="liver" />} />
+        <Route path='/diabetes' element={<Input path="diabetes" setResult={setResult} />} />
+        <Route path="/heart" element={<Input path="heart" />} />
+        <Route path="/kidney" element={<Input path="kidney" />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/offer' element={<Offer />} />
+        <Route path='/tech' element={<Technology />} />
+        <Route path='/Result' element={<Result result={result} />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
