5305e0fd3730de56d44063229540e396b2a97f8d
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-12 18:34:35 +0530

    Team page tamplating done

---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar'
+import { Footer } from '../Components/Footer'
+import { Team } from '../Components/Team'
+
+export const About = () => {
+    return (
+        <>
+            <Navbar />
+            <Team />
+            <Footer />
+        </>
+    )
+}
