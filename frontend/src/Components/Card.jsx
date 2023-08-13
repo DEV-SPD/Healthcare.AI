7b19c269696bc92882cb8638db43b3f0a03c9ff0
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-13 13:46:25 +0530

    bug fixed

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -42,7 +42,7 @@ export const Card = ({ refe }) => {
                                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Liver Disease Diagnosis </h1>
                                     <p className="leading-relaxed mb-3">Empower your health decisions with our Heart Disease Diagnosis Tool – a cutting-edge solution for assessing your risk of cardiovascular issues.</p>
                                     <div className="flex items-center flex-wrap ">
-                                        <Link to="/liver" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Predict Now
+                                        <Link to="/liver" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Diagnose Now
                                             <svg className="w-4 h-4 ml-2 group-hover:translate-x-3 duration-300 " viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" >
                                                 <path d="M5 12h14"></path>
                                                 <path d="M12 5l7 7-7 7"></path>
