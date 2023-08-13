aa2dc4d3904885069b3f991bfe97a4cd1b68c22d
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-08-13 13:15:25 +0530

    diagnose done

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -22,9 +22,9 @@ export const Card = ({ refe }) => {
                                 <div className="p-6">
                                     <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Category</h2>
                                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Thorax Disease Diagnosis</h1>
-                                    <p className="leading-relaxed mb-3">Empower your health decisions with our Heart Disease Predictor: Assess your risk accurately and make informed choices for a heart-healthy life.</p>
+                                    <p className="leading-relaxed mb-3">The thorax disease diagnosis tool assists medical professionals in accurately identifying and evaluating various thoracic region disorders using patient data and medical imaging.</p>
                                     <div className="flex items-center flex-wrap ">
-                                        <Link to="https://dev-spd-cardiovasucular-disease-app-eb6laq.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Predict Now
+                                        <Link to="https://dev-spd-cardiovasucular-disease-app-eb6laq.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Diagnose Now
                                             <svg className="w-4 h-4 ml-2 group-hover:translate-x-3 duration-300 " viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" >
                                                 <path d="M5 12h14"></path>
                                                 <path d="M12 5l7 7-7 7"></path>
@@ -60,7 +60,7 @@ export const Card = ({ refe }) => {
                                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Diabetes Diagnosis</h1>
                                     <p className="leading-relaxed mb-3">Empower your health decisions with our Diabetes Diagnosis Tool: Accurate risk assessment for early detection and prevention.</p>
                                     <div className="flex items-center flex-wrap ">
-                                        <Link to="https://dev-spd-diabetes-diagnosis-app-2lu3nn.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Predict Now
+                                        <Link to="https://dev-spd-diabetes-diagnosis-app-2lu3nn.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Diagnose Now
                                             <svg className="w-4 h-4 ml-2 group-hover:translate-x-3 duration-300 " viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" >
                                                 <path d="M5 12h14"></path>
                                                 <path d="M12 5l7 7-7 7"></path>
@@ -78,7 +78,7 @@ export const Card = ({ refe }) => {
                                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Kidney Disease Diagnosis</h1>
                                     <p className="leading-relaxed mb-3">An advanced tool for predicting kidney disease risk by analyzing relevant health factors and providing early detection insights</p>
                                     <div className="flex items-center flex-wrap ">
-                                        <Link to="/kidney" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Predict Now
+                                        <Link to="/kidney" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Diagnose Now
                                             <svg className="w-4 h-4 ml-2 group-hover:translate-x-3 duration-300 " viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" >
                                                 <path d="M5 12h14"></path>
                                                 <path d="M12 5l7 7-7 7"></path>
@@ -96,7 +96,7 @@ export const Card = ({ refe }) => {
                                     <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Heart Disease Diagnosis</h1>
                                     <p className="leading-relaxed mb-3">Empower your health decisions with our Heart Disease Diagnosis: Assess your risk accurately and make informed choices for a heart-healthy life.</p>
                                     <div className="flex items-center flex-wrap ">
-                                        <Link to="https://dev-spd-cardiovasucular-disease-app-eb6laq.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Predict Now
+                                        <Link to="https://dev-spd-cardiovasucular-disease-app-eb6laq.streamlit.app/" target="_blank" className="text-[#3A8799] inline-flex items-center md:mb-2 lg:mb-0 hover:bg-opacity-[0.1] hover:text-black rounded-lg group hover:scale-110 duration-300 ">Diagnose Now
                                             <svg className="w-4 h-4 ml-2 group-hover:translate-x-3 duration-300 " viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" >
                                                 <path d="M5 12h14"></path>
                                                 <path d="M12 5l7 7-7 7"></path>
--- frontend/src/Components/Kidney.jsx
+++ frontend/src/Components/Kidney.jsx
@@ -227,7 +227,7 @@ export const Kidney = ({ setResult }) => {
                             </div>
                             <div className="submit w-full flex justify-center">
                                 <button type="submit" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-[50%]" >
-                                    Predict Now
+                                    Diagnose Now
                                 </button>
                             </div>
 
